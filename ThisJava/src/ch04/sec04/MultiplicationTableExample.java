0d6e5c4abff47e35f03f5db26b65362481e0c838
hyeji111544 <hyeji111544@gmail.com>
2024-01-20 01:26:18 +0900

    책 예제 추가

---
+++ ThisJava/src/ch04/sec04/MultiplicationTableExample.java
@@ -0,0 +1,14 @@
+package ch04.sec04;
+
+public class MultiplicationTableExample {
+	public static void main(String[] args) {
+		for(int m=2; m<=9; m++) {
+			System.out.println("*** " + m + "단 ***");
+			
+			for(int i = 1; i<=9; i++) {
+				System.out.println(m +" X "+ i + " = " + (m*i));
+			}
+		}
+	}
+
+}
